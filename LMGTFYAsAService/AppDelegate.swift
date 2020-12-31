2852f50dfc7eece8d257a2b0c129c58a72e9a29e
Linus S <LinusS1@users.noreply.github.com>
2020-12-31 11:47:43 -0800

    Initial Commit

---
+++ LMGTFYAsAService/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LMGTFYAsAService
+//
+//  Created by Linus Skucas on 12/31/20.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
